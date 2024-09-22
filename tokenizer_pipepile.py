6fb04db44c64da16eacc43a873f699f9141ab0e6
Станислав <stanislavskuratovich37@gmail.com>
2024-09-22 20:40:40 +0300

    example of splitting into tokens in tokenization part

--- tokenizer_pipepile.py
+++ tokenizer_pipepile.py
@@ -2,5 +2,9 @@ from transformers import AutoTokenizer
 
 model = 'bert-base-uncased'
 tokenizer = AutoTokenizer.from_pretrained(model)
-inputs = tokenizer('It is time to tokenize')
-print(inputs['input_ids'])
+tokens = tokenizer.tokenize('It is time to tokenize')  # splitting the text to special tokens
+print(tokens)
+
+# in this example tokenizer make following actions to split the text to tokens
+# -converting the text to lowercase
+# -splitting the text using the special rules of subword-based tokenization
